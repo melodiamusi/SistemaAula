b3a7ac57ebbce458d953906c6e903aabdc5da4c8
melodiamusi <149274532+melodiamusi@users.noreply.github.com>
2026-07-19 04:57:48 +0200

    codigo completo

---
+++ Src/SistemaAula.Domain/Modelos/AulaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaAula.Domain.Modelos
+{
+    public class AulaDTO
+    {
+        public string Nombre { get; set; } = string.Empty;
+
+        public int Capacidad { get; set; }
+
+        public string Ubicacion { get; set; } = string.Empty;
+    }
+}
---
+++ Src/SistemaAula.Domain/Modelos/EstudianteDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace SistemaAula.Domain.Modelos
+{
+    public class EstudianteDTO
+    {
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Apellido { get; set; } = string.Empty;
+
+        public string Matricula { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
+
